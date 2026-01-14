ef972e75b15e38f1b50f78720b5ff75f70b96f7f
hasmukhsingh9901 <hasmukhsingh90@gmail.com>
2026-01-14 11:36:05 +0530

    Fix : removed commented

--- src/pages/masters/user-management/RoleWiseRightsAssign.tsx
+++ src/pages/masters/user-management/RoleWiseRightsAssign.tsx
@@ -79,7 +79,6 @@ const RoleWiseRightsAssign: React.FC = () => {
 
         <div className="flex gap-3">
           <Button type="submit" label="Assign Rights" icon="pi pi-save" className="p-button-primary" />
-          {/* <Button type="button" label="Cancel" icon="pi pi-times" className="p-button-secondary" /> */}
           <Button type="button" label="Reset" icon="pi pi-refresh" className="p-button-outlined" onClick={() => setFormData({ roleName: '', selectedForms: [] })} />
         </div>
       </form>
